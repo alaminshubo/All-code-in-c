2b08537cab6a1881d1f1ab45ad6dbf425be4c6f0
Md. Al Amin <168575309+alaminshubo@users.noreply.github.com>
2024-05-09 21:35:18 +0600

    Add files via upload

---
+++ passing_array_to_function.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void display(int x[])
+{
+    int i=0;
+    for(i=0; i<5; i++)
+    {
+        printf("%d\n",x[i]);
+    }
+}
+int main()
+{
+    int num[]={10,20,30,40,50};
+    display (num);
+}
